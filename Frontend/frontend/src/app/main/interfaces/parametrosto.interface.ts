9a015d766e7b2c648f03c8a1d3f8c415073d64b9
AngelicaCalderon <angelus26110747@gmail.com>
2021-11-17 10:41:05 -0500

    Actualizacion parametros

--- Frontend/frontend/src/app/main/interfaces/parametrosto.interface.ts
+++ Frontend/frontend/src/app/main/interfaces/parametrosto.interface.ts
@@ -1,17 +1,17 @@
 export interface IParametrosTO{
-    tipo_carro: Int;
-    tipo_moto: Int;
-    tipo_bicicleta: Int;
-    tpm_carro: Int;
-    tpm_moto: Int;
-    tpm_bicicleta: Int
+    tipo_carro: Number;
+    tipo_moto: Number;
+    tipo_bicicleta: Number;
+    tpm_carro: Number;
+    tpm_moto: Number;
+    tpm_bicicleta: Number
 }
 
 export class ParametrosTO implements IParametrosTO {
-  public tipo_carro: Int = 0;
-  public tipo_moto: Int = 0;
-  public tipo_bicicleta: Int = 0;
-  public tpm_carro: Int = 0;
-  public tpm_moto: Int = 0;
-  public tpm_bicicleta: Int = 0;
+  public tipo_carro: Number = 0;
+  public tipo_moto: Number = 0;
+  public tipo_bicicleta: Number = 0;
+  public tpm_carro: Number = 0;
+  public tpm_moto: Number = 0;
+  public tpm_bicicleta: Number = 0;
 }
