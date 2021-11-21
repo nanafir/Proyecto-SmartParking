9926fe50dda89ec8331f049cd71b92b2631ecd35
AngelicaCalderon <angelus26110747@gmail.com>
2021-11-20 19:46:30 -0500

    Actualizacion 7:45

--- Frontend/frontend/src/app/main/plazas/service/plazas.service.ts
+++ Frontend/frontend/src/app/main/plazas/service/plazas.service.ts
@@ -10,6 +10,7 @@ import { PlazasTO } from '../../interfaces/plazasto.interface';
 export class PlazasService {
 
   constructor(private _http: HttpClient) { }
+
   listarPlazas(): Observable<PlazasTO[]> {
     return this._http.get<PlazasTO[]>("http://127.0.0.1:1400/api/plazas");
   }
